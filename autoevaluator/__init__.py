8a87e1666f4ea81c887ca9783622fdbb694926ad
Darveen Vijayan <43519646+darveenvijayan@users.noreply.github.com>
2024-10-03 03:20:20 +0000

    created evals folder

--- autoevaluator/__init__.py
+++ autoevaluator/__init__.py
@@ -1,1 +1,2 @@
 from .client import setup_client
+from .evals import *
---
+++ autoevaluator/evals.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from openai import OpenAI, AzureOpenAI
+
+import instructor
+
+
+
