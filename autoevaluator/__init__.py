1e8af9bbda12138941db5cfad628d4639c1645cb
Darveen Vijayan <43519646+darveenvijayan@users.noreply.github.com>
2024-10-12 08:37:06 +0000

    push evaluate v2

--- autoevaluator/__init__.py
+++ autoevaluator/__init__.py
@@ -1,6 +1,6 @@
 from .client import setup_client
 from .simplify import text_simplifier
-from .eval import evaluate
-# from .eval_v2 import evaluate 
+# from .eval import evaluate
+from .eval_v2 import evaluate 
 from typing import Dict
 from openai import OpenAI, AzureOpenAI
